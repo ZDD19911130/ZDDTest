732d33681528cd362b6e95d213adb55f7dde3bcc
zdd <1933562120@qq.com>
2018-02-05 11:17:37 +0800

    ZDDTest

--- ZDDTest/ViewController.m
+++ ZDDTest/ViewController.m
@@ -6,17 +6,42 @@
 //  Copyright © 2017年 com.micomme.breathmachine. All rights reserved.
 //
 
+
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic ,strong) UITableView * tableView;
 @end
 
 @implementation ViewController
 
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) style:UITableViewStylePlain];
+        _tableView.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.4];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.tableFooterView = [[UIView alloc] init];
+    }
+    return _tableView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self.view addSubview:self.tableView];
+    
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [[UITableViewCell alloc] init];
+    return cell;
 }
 
 
